115828527644ee4c68b6c3014a8c670b4c46c3ce
cosmos_1885 <dmsrkql104321@gmail.com>
2023-11-13 20:08:00 +0900

    FEAT: Screen 생성

---
+++ screens/Food_Chatbot.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const Food_Chatbot = () => {
+  return <Text>Food Chatbot</Text>;
+};
+
+export default Food_Chatbot;
---
+++ screens/Medicine_Analysis.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const Medicine_Analysis = () => {
+  return <Text>Medicine Analysis</Text>;
+};
+
+export default Medicine_Analysis;
