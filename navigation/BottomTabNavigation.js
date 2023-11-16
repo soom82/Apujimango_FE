0268d18c59da20ab3492215fb1e7bac2f68a2b25
cosmos_1885 <dmsrkql104321@gmail.com>
2023-11-16 17:50:09 +0900

    feat: StackNavigation 추가

--- navigation/BottomTabNavigation.js
+++ navigation/BottomTabNavigation.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { Platform } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Iconify } from "react-native-iconify";
 
@@ -8,17 +9,32 @@ import Home from "../screens/Home";
 import Food_Chatbot from "../screens/Food_Chatbot";
 import Medicine_Analysis from "../screens/Medicine_Analysis";
 import AI_Question from "../screens/AI_Question";
-import Disease_Encyclopedia from "../screens/Disease_Encyclopedia";
+import Disease_Encyclopedia1 from "../screens/Disease_Encyclopedia1";
+import Disease_Encyclopedia2 from "../screens/Disease_Encyclopedia2";
 
 // const BottomTabNavigationIcon = ({ iconName, color, size }) => {
 //   return <Iconify icon={iconName} color={color} size={size} />;
 // };
 
-const AlertButton = () => {
-  return <Iconify icon="akar-icons:bell" />;
-};
-
 const BottomTabNavigator = createBottomTabNavigator();
+const StackNavigator = createStackNavigator();
+
+const Disease_Encyclopedia_Stack = () => {
+  return (
+    <StackNavigator.Navigator>
+      <StackNavigator.Screen
+        name="질환백과1"
+        component={Disease_Encyclopedia1}
+        options={{ headerShown: false }}
+      />
+      <StackNavigator.Screen
+        name="질환백과2"
+        component={Disease_Encyclopedia2}
+        options={{ headerShown: false }}
+      />
+    </StackNavigator.Navigator>
+  );
+};
 
 const BottomTabNavigation = () => {
   return (
@@ -33,13 +49,14 @@ const BottomTabNavigation = () => {
         component={Home}
         options={{
           tabBarIcon: () => <Iconify icon="lucide:baby" size={24} />,
-          headerRight: (props) => <AlertButton {...props} />,
+          // headerRight: (props) => <AlertButton {...props} />,
+          headerShown: false,
           tabBarStyle: {
             height: 63,
             borderTopLeftRadius: 15,
             borderTopRightRadius: 15,
             borderTopWidth: 0,
-            elevation: 30,
+            elevation: 20,
           },
         }}
       />
@@ -48,6 +65,7 @@ const BottomTabNavigation = () => {
         component={Food_Chatbot}
         options={{
           tabBarIcon: () => <Iconify icon="ep:bowl" size={25} />,
+          headerShown: false,
         }}
       />
       <BottomTabNavigator.Screen
@@ -57,6 +75,7 @@ const BottomTabNavigation = () => {
           tabBarIcon: () => (
             <Iconify icon="healthicons:medicines-outline" size={28} />
           ),
+          headerShown: false,
         }}
       />
       <BottomTabNavigator.Screen
@@ -64,13 +83,16 @@ const BottomTabNavigation = () => {
         component={AI_Question}
         options={{
           tabBarIcon: () => <Iconify icon="solar:cat-linear" size={29} />,
+          headerShown: false,
         }}
       />
       <BottomTabNavigator.Screen
         name="질환백과"
-        component={Disease_Encyclopedia}
+        component={Disease_Encyclopedia_Stack}
         options={{
           tabBarIcon: () => <Iconify icon="iconoir:book" size={24} />,
+          tabBarHideOnKeyboard: true,
+          headerShown: false,
         }}
       />
     </BottomTabNavigator.Navigator>
