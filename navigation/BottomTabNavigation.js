12c5fdccb68f3b33c5d591574b8e6143857e5c1f
cosmos_1885 <dmsrkql104321@gmail.com>
2023-11-21 01:13:28 +0900

    feat: medicine_analysis stacknavigation 추가

--- navigation/BottomTabNavigation.js
+++ navigation/BottomTabNavigation.js
@@ -8,10 +8,14 @@ import { Iconify } from "react-native-iconify";
 import Home from "../screens/Home";
 import Child_Management from "../screens/Child_Management";
 import Food_Chatbot from "../screens/Food_Chatbot";
-import Medicine_Analysis from "../screens/Medicine_Analysis";
+import Medicine_Analysis1 from "../screens/Medicine_Analysis1";
+import Medicine_Analysis2 from "../screens/Medicine_Analysis2";
+import Medicine_Analysis3 from "../screens/Medicine_Analysis3";
+import Medicine_Analysis4 from "../screens/Medicine_Analysis4";
 import AI_Question from "../screens/AI_Question";
 import Disease_Encyclopedia1 from "../screens/Disease_Encyclopedia1";
 import Disease_Encyclopedia2 from "../screens/Disease_Encyclopedia2";
+import { Icon } from "react-native-elements";
 
 // const BottomTabNavigationIcon = ({ iconName, color, size }) => {
 //   return <Iconify icon={iconName} color={color} size={size} />;
@@ -31,6 +35,33 @@ const Home_Stack = () => {
       <StackNavigator.Screen
         name="아이 관리"
         component={Child_Management}
+        options={{ headerShown: false }}
+      />
+    </StackNavigator.Navigator>
+  );
+};
+
+const Medicine_Analysis_Stack = () => {
+  return (
+    <StackNavigator.Navigator>
+      <StackNavigator.Screen
+        name="처방전 등록"
+        component={Medicine_Analysis1}
+        options={{ headerShown: false }}
+      />
+      <StackNavigator.Screen
+        name="직접 등록"
+        component={Medicine_Analysis2}
+        options={{ headerShown: false }}
+      />
+      <StackNavigator.Screen
+        name="약 선택"
+        component={Medicine_Analysis3}
+        options={{ headerShown: false }}
+      />
+      <StackNavigator.Screen
+        name="중간 알림"
+        component={Medicine_Analysis4}
         options={{ headerShown: false }}
       />
     </StackNavigator.Navigator>
@@ -60,38 +91,55 @@ const BottomTabNavigation = () => {
       initialRouteName="홈"
       screenOptions={{
         tabBarActiveTintColor: "#FBB144",
+        tabBarStyle: {
+          elevation: 20,
+          height: 60,
+          borderTopLeftRadius: 15,
+          borderTopRightRadius: 15,
+          borderTopWidth: 0,
+        },
+        tabBarIconStyle: {
+          position: "absolute",
+          top: 25,
+        },
+        tabBarLabelStyle: {
+          position: "absolute",
+          top: 40,
+          fontSize: 10,
+        },
       }}
     >
       <BottomTabNavigator.Screen
         name="홈"
         component={Home_Stack}
         options={{
-          tabBarIcon: () => <Iconify icon="lucide:baby" size={24} />,
+          tabBarIcon: ({ size, color }) => (
+            <Iconify icon="lucide:baby" size={size} color={color} />
+          ),
           // headerRight: (props) => <AlertButton {...props} />,
           headerShown: false,
-          tabBarStyle: {
-            elevation: 20,
-            height: 63,
-            borderTopLeftRadius: 15,
-            borderTopRightRadius: 15,
-            borderTopWidth: 0,
-          },
         }}
       />
       <BottomTabNavigator.Screen
         name="식품 챗봇"
         component={Food_Chatbot}
         options={{
-          tabBarIcon: () => <Iconify icon="ep:bowl" size={25} />,
+          tabBarIcon: ({ size, color }) => (
+            <Iconify icon="ep:bowl" size={size} color={color} />
+          ),
           headerShown: false,
         }}
       />
       <BottomTabNavigator.Screen
         name="약 분석"
-        component={Medicine_Analysis}
+        component={Medicine_Analysis_Stack}
         options={{
-          tabBarIcon: () => (
-            <Iconify icon="healthicons:medicines-outline" size={28} />
+          tabBarIcon: ({ size, color }) => (
+            <Iconify
+              icon="healthicons:medicines-outline"
+              size={size}
+              color={color}
+            />
           ),
           headerShown: false,
         }}
@@ -100,7 +148,9 @@ const BottomTabNavigation = () => {
         name="AI 질문"
         component={AI_Question}
         options={{
-          tabBarIcon: () => <Iconify icon="solar:cat-linear" size={29} />,
+          tabBarIcon: ({ size, color }) => (
+            <Iconify icon="solar:cat-linear" size={size} color={color} />
+          ),
           headerShown: false,
         }}
       />
@@ -108,7 +158,9 @@ const BottomTabNavigation = () => {
         name="질환백과"
         component={Disease_Encyclopedia_Stack}
         options={{
-          tabBarIcon: () => <Iconify icon="iconoir:book" size={24} />,
+          tabBarIcon: ({ size, color }) => (
+            <Iconify icon="iconoir:book" size={size} color={color} />
+          ),
           tabBarHideOnKeyboard: true,
           headerShown: false,
         }}
